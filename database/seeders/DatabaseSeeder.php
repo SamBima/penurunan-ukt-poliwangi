0f4f13bd8fdefbb0acca50e111b51d2aca1e6c11
SamBima <bsamudra267@gmail.com>
2026-07-15 23:19:57 +0700

    chore: comment out MahasiswaSeeder to clean up database student data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call(JurusanProdiSeeder::class);
         $this->call(UserSeeder::class);
-        $this->call(MahasiswaSeeder::class);
+        // $this->call(MahasiswaSeeder::class);
     }
 }
