98bfaafcfc22ce2329d35a6ff89364ecdc7c014c
SamBima <bsamudra267@gmail.com>
2026-07-15 19:14:50 +0700

    feat: expand MahasiswaSeeder to dynamically generate 60 more realistic student records matching the second image

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,5 @@ class DatabaseSeeder extends Seeder
 
         $this->call(JurusanProdiSeeder::class);
         $this->call(UserSeeder::class);
-        $this->call(MahasiswaSeeder::class);
     }
 }
